97549cefb115a90cad71df2c67b4c9229d1e34e3
moghya <sawantshubham571@gmail.com>
2022-10-23 17:07:05 +0530

    Add exectuable for RockPaperScissorsHandGame

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+#include "imc_rps_game/imc_rock_paper_scissors_hand_game.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string playerName;
+    uint numberOfRounds;
+    std::cout << "Enter name of player: ";
+    std::cin >> playerName;
+    std::cout << "\nEnter number of rounds: ";
+    std::cin >> numberOfRounds;
+    auto game = imc::RockPaperScissorsHandGame(playerName, numberOfRounds);
+    game.Start();
     return 0;
 }
